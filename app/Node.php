1985d8e735a395e1cd92a44521b97d7267b62e90
SkepticalHippo <edvinas@fluxministry.com>
2015-05-27 12:21:59 +0100

    Forumo skilciu parent ir child relatioships ir rodymas.

--- app/Node.php
+++ app/Node.php
@@ -1,9 +1,20 @@
 <?php namespace maze;
 
 use Illuminate\Database\Eloquent\Model;
+use maze\Node;
 
 class Node extends Model {
 
-	//
+	public function topics() {
+		return $this->hasMany('maze\Topic');
+	}
+
+	public function children() {
+		return $this->hasMany('maze\Node', 'parent_node')->orderBy('order', 'ASC');
+	}
+
+	public static function parents() {
+		return Node::whereNull('parent_node')->orderBy('order', 'ASC')->get();
+	}
 
 }
