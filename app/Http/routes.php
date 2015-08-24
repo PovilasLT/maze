59cb213644c356482fa6a66d2587225b2e908c31
Stanislovas Janonis <reflexltu@gmail.com>
2015-08-24 15:54:28 +0300

    Confer routes fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,7 +36,7 @@ require_once('Routes/tv.routes.php');
 require_once('Routes/blog.routes.php');
 
 //Confer
-//require_once('Routes/confer.routes.php');
+require_once('Routes/confer.routes.php');
 
 //Senų route 301 redirectai.
 require_once('Routes/legacy.routes.php');
