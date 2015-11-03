d050fb410a7c9ec8c98b76c18c023a68bf8f729c
Stanislovas <stanislovas.janonis@gmail.com>
2015-11-03 19:32:03 +0200

    fix all dat shitty stufF

--- resources/views/vendor/confer/userlist.blade.php
+++ resources/views/vendor/confer/userlist.blade.php
@@ -1,30 +1,30 @@
 <h2>Users</h2>
-<small>Find people available to converse with, and those not currently online.</small>
+<small>Visų narių sąrašas, kuriame gali rasti sau pašnekovą.</small>
 
 <ul class="confer-user-list confer-online-list">
-<h3>Active</h3>
+<h3>Prisijungę</h3>
 @if ( ! $users['online']->isEmpty())
 	@foreach ($users['online'] as $user)
 		<li data-userId="{{ $user->id }}">
 			<img class="confer-user-avatar" src="{{ url('/') . config('confer.avatar_dir') . $user->avatar }}">
-			<span class="confer-user-name">{{ $user->name }}</span>
+			<span class="confer-user-name">{{ $user->username }}</span>
 		</li>
 	@endforeach
 @else
-	<p>There are no users online (apart from you!)</p>
+	<p>Esi vienintelis prisijungęs žmogus!</p>
 @endif
 </ul>
 
 <ul class="confer-user-list confer-not-online-list">
-<h3>Offline</h3>
+<h3>Atsijungę</h3>
 @if ( ! $users['offline']->isEmpty())
 	@foreach ($users['offline'] as $user)
 		<li data-userId="{{ $user->id }}">
 			<img class="confer-user-avatar" src="{{ url('/') . config('confer.avatar_dir') . $user->avatar }}">
-			<span class="confer-user-name">{{ $user->name }}</span>
+			<span class="confer-user-name">{{ $user->username }}</span>
 		</li>
 	@endforeach
 @else
-	<p>It looks like everyone is online... that's weird.</p>
+	<p>Visi nariai yra prisijungę :o</p>
 </ul>
 @endif
