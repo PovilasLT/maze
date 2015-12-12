e2cbf0fa54b8325e55b1bd14b99c99b1e2b2d635
="Justas <justas963@gmail.com>
2015-12-12 15:39:33 +0200

    Pagaliau išverčiau. (Veluoju tik 9 dienas. Beat that Yiin)

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
 	|
 	*/
 
-	"password" => "Passwords must be at least six characters and match the confirmation.",
-	"user" => "We can't find a user with that e-mail address.",
-	"token" => "This password reset token is invalid.",
-	"sent" => "We have e-mailed your password reset link!",
-	"reset" => "Your password has been reset!",
+	"password" => "Slaptaþodá turi sudaryti bent ðeði simboliai.",
+	"user" => "Narys su nurodytu el. paðtu neegzistuoja.",
+	"token" => "Slaptaþodþio keitimo kodas negaliojantis.",
+	"sent" => "Nuoroda slaptaþodþio keitimui buvo iðsiøst jums á el. paðtà!",
+	"reset" => "Jûsø slaptaþodis atnaujintas!",
 
 ];
