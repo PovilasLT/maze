fb7b41d2cf88c47da4f5bf46398c4beac733d7d5
SkepticalHippo <edvinasrp@gmail.com>
2016-01-01 22:56:27 +0000

    pridetas User dependency

--- app/Events/UserWasNotified.php
+++ app/Events/UserWasNotified.php
@@ -5,6 +5,7 @@ namespace maze\Events;
 use maze\Events\Event;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use maze\User;
 
 class UserWasNotified extends Event
 {
