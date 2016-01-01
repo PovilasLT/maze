fbd2c84fc466d421e568abe99120350cddd05842
SkepticalHippo <edvinas@fluxministry.com>
2016-01-01 21:07:22 +0000

    naujienu siuntimo tikrinimas sukurus nauja tema

--- app/Events/TopicWasCreated.php
+++ app/Events/TopicWasCreated.php
@@ -5,19 +5,36 @@ namespace maze\Events;
 use maze\Events\Event;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use maze\Events\NewsWasPosted;
+
+use maze\User;
+use maze\Topic;
 
 class TopicWasCreated extends Event
 {
     use SerializesModels;
+
+    public $topic;
+    public $user;
+    public $notifiable;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Topic $topic, User $user)
     {
-        //
+        $this->topic = $topic;
+        $this->user = $user;
+        $this->notifiable = $topic;
+
+        //patikrinam ar tema yra naujiena.
+        if($topic->node_id == 15)
+        {
+            event(new NewsWasPosted($topic));
+        }
+
     }
 
     /**
