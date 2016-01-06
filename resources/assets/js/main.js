5f033f83db9b34413b4681abf53043bdd6721624
SkepticalHippo <edvinasrp@gmail.com>
2016-01-06 09:28:29 +0000

    'buginantis' isskleidimas

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -6,17 +6,22 @@ $(document).ready(function() {
 	$('#toggle-sidebar').click(function() {
 	var main_content = $('.main-content');
 	var main_sidebar = $('.main-sidebar');
+	var footer = $('footer');
 
 	if(main_content.hasClass('is_visible'))
 	{
+		footer.hide();
 		main_content.slideUp('', function() {
 			main_sidebar.removeClass('hidden-xs').removeClass('hidden-sm');
+			footer.show();
 		}).removeClass('is_visible');
 	}
 	else
 	{
+		footer.hide();
 		main_content.slideDown('', function() {
 			main_sidebar.addClass('hidden-xs').addClass('hidden-sm');
+			footer.show();
 		}).addClass('is_visible');
 	}
 	});
