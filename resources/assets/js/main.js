6320c1fae375711725504e560a86ed1ecd3f1d14
SkepticalHippo <edvinasrp@gmail.com>
2016-01-06 09:21:40 +0000

    mobilus dizainas ir #18

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-  $('pre code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
+	$('pre code').each(function(i, block) {
+		hljs.highlightBlock(block);
+	});
+
+	$('#toggle-sidebar').click(function() {
+	var main_content = $('.main-content');
+	var main_sidebar = $('.main-sidebar');
+
+	if(main_content.hasClass('is_visible'))
+	{
+		main_content.slideUp('', function() {
+			main_sidebar.removeClass('hidden-xs').removeClass('hidden-sm');
+		}).removeClass('is_visible');
+	}
+	else
+	{
+		main_content.slideDown('', function() {
+			main_sidebar.addClass('hidden-xs').addClass('hidden-sm');
+		}).addClass('is_visible');
+	}
+	});
 });
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -105,25 +105,6 @@
 			<div class="current-date-invisible" style="display: none;">{{ \Carbon\Carbon::now() }}</div>
 		</footer>
 		<script src="/js/scripts.js"></script>
-		<script type="text/javascript">
-			$('#toggle-sidebar').click(function() {
-				var main_content = $('.main-content');
-				var main_sidebar = $('.main-sidebar');
-
-				if(main_content.hasClass('is_visible'))
-				{
-					main_content.slideUp('', function() {
-						main_sidebar.removeClass('hidden-xs').removeClass('hidden-sm');
-					}).removeClass('is_visible');
-				}
-				else
-				{
-					main_content.slideDown('', function() {
-						main_sidebar.addClass('hidden-xs').addClass('hidden-sm');
-					}).addClass('is_visible');
-				}
-			});
-		</script>
 		@yield('scripts')
 	</body>
 </html>
