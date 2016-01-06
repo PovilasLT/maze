94522b070b72b6529baebd947152665ecc170e69
SkepticalHippo <edvinasrp@gmail.com>
2016-01-06 13:00:09 +0000

    error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -25,7 +25,7 @@ class Handler extends ExceptionHandler {
 	public function render($request, Exception $e)
 	{
 
-		return parent::render($request, $e);
+		// return parent::render($request, $e);
 
 		if($this->isHttpException($e))
 		{
