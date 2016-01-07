f581dda21ebc2937ffc4765e3d206cefc383d98e
SkepticalHippo <edvinasrp@gmail.com>
2016-01-07 18:04:26 +0000

    55

--- resources/views/errors/internal.blade.php
+++ resources/views/errors/internal.blade.php
@@ -8,7 +8,7 @@
 
 	<h1>Oi! Kažkas ne taip...</h1>
 
-	<p>Įvyko klaida mūsų pusėje. Ši klaida buvo užfiksuota ir galiausiai bus patikrinta administracijos. Jeigu klaida skubi ir manote, kad be greito pataisymo gyventi neįmanoma - informkuotie administraciją.</p>
+	<p>Įvyko klaida mūsų pusėje. Ši klaida buvo užfiksuota ir galiausiai bus patikrinta administracijos. Jeigu klaida skubi ir manote, kad be greito pataisymo gyventi neįmanoma - informuokite administraciją.</p>
 
 @stop
 
