1b85825d6aae7862a7963499b32a5ea444987609
SkepticalHippo <edvinasrp@gmail.com>
2016-01-07 20:47:53 +0000

    aktyvios temos pažymėjimas #14

--- resources/views/panels/nodes.blade.php
+++ resources/views/panels/nodes.blade.php
@@ -5,10 +5,14 @@
   <div class="panel-body">
 	<ul class="node-list">
     @foreach(maze\Node::parents() as $parent)
-      <li id="parent-node-{{ $parent->id }}" @if(isset($expandable) && $expandable == $parent->id) class="is-expanded" @endif><i class="fa fa-plus parent-icon" id="{{$parent->id}}"></i><a href="{{ route('node.show', $parent->slug) }}">{{ $parent->name }}</a></li>
+      <li id="parent-node-{{ $parent->id }}" class="
+      @if(isset($expandable) && $expandable == $parent->id) is-expanded @endif 
+      @if(isset($node) && $node->id == $parent->id) active-node @endif
+      ">
+      <i class="fa fa-plus parent-icon" id="{{$parent->id}}"></i><a href="{{ route('node.show', $parent->slug) }}">{{ $parent->name }}</a></li>
       <ul class="child-node-list parent-node-collection-{{ $parent->id }}">
         @foreach($parent->children as $child)
-          <li>
+          <li @if(isset($node) && $node->id == $child->id) class="active-node" @endif >
             <a href="{{ route('node.show', $child->slug) }}">{{ $child->name }}</a>
           </li>
         @endforeach
