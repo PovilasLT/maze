0b62eb2c253c296f2f94cb46dfb5854f22ab96e7
SkepticalHippo <edvinasrp@gmail.com>
2016-01-12 20:58:12 +0000

    font'as iškeltas į CSS

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,8 +14,6 @@
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
-
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<nav class="navbar navbar-default" role="navigation">
