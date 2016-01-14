b933635ccf510ceefe4c86669864b8eed5c39eb5
Stanislovas <stanislovas.janonis@gmail.com>
2016-01-14 20:55:12 +0200

    Mygtukas kategorijose sekti/nebesekti

--- app/Http/Controllers/FrontPageNodesController.php
+++ app/Http/Controllers/FrontPageNodesController.php
@@ -17,7 +17,23 @@ class FrontPageNodesController extends Controller
         $this->middleware('loggedIn');
     }
 
-    public function toggle(Request $request) {
+    public function toggle($id) {
+        $user = Auth::user();
+        $state = FrontPageNode::where('user_id', $user->id)->where('node_id', $id)->exists();
+
+        if($state) {
+            FrontPageNode::where('user_id', $user->id)->where('node_id', $id)->delete();
+        }
+        else {
+            FrontPageNode::firstOrNew([
+                'user_id' => $user->id,
+                'node_id' => $id
+            ])->save();
+        }
+        return redirect()->back();
+    }
+
+    public function update(Request $request) {
         $node_id = $request->input('node_id');
         $state = $request->input('state');
         $user = Auth::user();
@@ -31,6 +47,5 @@ class FrontPageNodesController extends Controller
         else {
             FrontPageNode::where('user_id', $user->id)->where('node_id', $node_id)->delete();
         }
-        return ['state' => $state];
     }
 }
--- app/Http/Routes/settings.routes.php
+++ app/Http/Routes/settings.routes.php
@@ -1,6 +1,11 @@
 <?php
 
-Route::post('/nodes/toggle', [
+Route::post('/nodes/update', [
 	'as' => 'nodes.update',
+	'uses' => 'FrontPageNodesController@update'
+]);
+
+Route::get('/node/toggle/{id}', [
+	'as' => 'nodes.toggle',
 	'uses' => 'FrontPageNodesController@toggle'
 ]);
