018e9378ff2730718283df28bd67142588b9c65f
SkepticalHippo <edvinasrp@gmail.com>
2016-01-16 05:52:47 +0000

    ssh server

--- notification.server.js
+++ notification.server.js
@@ -1,17 +1,7 @@
-var fs = require('fs');
-
-var ssl_conf = require('./ssl.json');
-
-var options = {
-  key: fs.readFileSync(ssl_conf.key),
-  cert: fs.readFileSync(ssl_conf.cert)
-};
-
-var app = require('https').createServer(function (req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end('<img src="https://s-media-cache-ak0.pinimg.com/736x/8b/e0/f8/8be0f8dd6d4a6ee1325c6d46297dd46e.jpg"></img>');
-});
-var io = require('socket.io')(app);
+var lex = require('letsencrypt-express').testing();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var Redis = require('ioredis');
 var redis = new Redis();
 var Imagemin = require('imagemin');
@@ -40,12 +30,16 @@ redis.on('pmessage', function(subscribed, channel, event) {
 	}
 });
 
+app.get('/', function (req, res) {
+	res.send('...');
+});
+
 io.sockets.on('connection', function (socket) {
   socket.on('join', function (data) {
     socket.join(data.id + '-' + data.secret);
   });
 });
 
-app.listen(6001, function() {
-    console.log('SERVERIS IJUNGTAS');
+lex.create('./letsencrypt.config', app).listen([], [6001], function () {
+  console.log("ENCRYPT __ALL__ THE DOMAINS!");
 });
