73ceb686bb93f203ecac04c9d51c531a128c14b2
SkepticalHippo <edvinasrp@gmail.com>
2016-01-30 12:54:35 +0000

    SSL notification serveris

--- notification.server.js
+++ notification.server.js
@@ -1,12 +1,16 @@
-var app = require('http').createServer(function (req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end('<img src="https://s-media-cache-ak0.pinimg.com/736x/8b/e0/f8/8be0f8dd6d4a6ee1325c6d46297dd46e.jpg"></img>');
-});
-var io = require('socket.io')(app);
+var lex = require('letsencrypt-express');
+var express = require('express');
+var app = express();
+var server = require('https').Server(app);
+var io = require('socket.io')(server);
 var Redis = require('ioredis');
 var redis = new Redis();
 var Imagemin = require('imagemin');
 var path = require('path');
+
+app.get('/', function (req, res) {
+  res.send('NOTIFICATIONS');
+});
 
 redis.psubscribe('*', function(err, count) {
 });
@@ -37,6 +41,10 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-app.listen(6001, function() {
-    console.log('SERVERIS IJUNGTAS');
+lex.create({
+	configDir: '/etc/letsencrypt',
+	onRequest: app,
+	letsencrypt: null
+}).listen([], [6001], function () {
+  console.log("SERVERIS IJUNGTAS!");
 });
