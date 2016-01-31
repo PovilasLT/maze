2ba4efd5fdc0275d2c72852829be77e1e02e544b
SkepticalHippo <edvinasrp@gmail.com>
2016-01-31 11:53:00 +0000

    is_blocked() -> is_blocked

--- resources/views/reply/controls.blade.php
+++ resources/views/reply/controls.blade.php
@@ -1,7 +1,7 @@
 <div class="panel-footer text-right">
 	<div class="btn-group" role="group" aria-label="...">
 		<a href="{{ route('reply.edit', $reply->id) }}"><button type="button" class="btn btn-xs btn-grey"><i class="fa fa-pencil"></i></button></a>
-		@if(!Auth::user()->can('manage_topics') && !$topic->is_blocked())
+		@if(!Auth::user()->can('manage_topics') && !$topic->is_blocked)
 		<a href="{{ route('reply.delete', $reply->id) }}"><button type="button" class="btn btn-xs btn-grey"><i class="fa fa-trash"></i></button></a>
 		@endif
 	</div>
