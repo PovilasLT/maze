3f25add3648bb80167f3cd6d8bfd6a47cc7bccfc
SkepticalHippo <edvinasrp@gmail.com>
2016-02-01 21:20:18 +0000

    pataisytas pranesimu emailo template

--- resources/views/emails/reply.blade.php
+++ resources/views/emails/reply.blade.php
@@ -1,7 +1,7 @@
 <p>Sveikas, {{{ $user }}}!</p>
 <p>Tema, kurią tu prenumeruoji, turi naują pranešimą:</p>
 <h2>{{ $title }}</h2>
-{{ $content }}
+{!! $content !!}
 <p><a href="{{ route('topic.show', [$slug, $id]) }}">Perskaityti ir atsakyti į temą.</a></p>
 <p><b>Dėmesio!</b> Po šio laiško, tema gali turėti ir daugiau naujų pranešimų.</p>
 <p>Pagarbiai,<br>
