35d6ca8b9d2f9a3086c8fbab4436249166aec4c8
SkepticalHippo <edvinasrp@gmail.com>
2016-03-28 16:23:30 +0100

    Notification evento pakeitimas.

--- app/Notifications/Notifier.php
+++ app/Notifications/Notifier.php
@@ -7,31 +7,21 @@ use Auth;
 class Notifier {
 	public static function notify($type, $object, $receiver)
 	{
-
 		$user = Auth::user();
-
 		$notification = Notification::create([
 			'user_id'		=> $receiver->id,
 			'from_id'		=> $user->id,
 			'object_type'	=> $type,
 			'object_id'		=> $object->id,
+			'is_read'		=> false,
 		]);
-
 
 		//jeigu notificationas ne mums patiems, padidinam notification count
 		//ir triggerinam send mail eventa.
-		if($receiver->id != $user->id)
-		{
-			$receiver->increment('notification_count');
-
-			if($receiver->email_replies)
-			{
-				event(new UserWasNotified($object, $receiver));
-			}
-
+		if($receiver->id != $user->id) {
+			event(new UserWasNotified($object, $receiver, $notification));
 		}
 
 		return $notification;
-
 	}
 }
