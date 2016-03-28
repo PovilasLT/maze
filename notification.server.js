898d5500c44ee1012657f656fa10df5302c6931e
SkepticalHippo <edvinasrp@gmail.com>
2016-03-28 16:28:10 +0100

    Pranešimų serveris.

--- notification.server.js
+++ notification.server.js
@@ -1,4 +1,4 @@
-var lex = require('letsencrypt-express');
+var lex = require('letsencrypt-express').testing();
 var express = require('express');
 var app = express();
 var redisConfig = require('./redis.json');
@@ -6,20 +6,23 @@ var redisConfig = require('./redis.json');
 var servers = lex.create({
 	configDir: '/etc/letsencrypt',
 	onRequest: app,
-}).listen([], [6001], function onListening() {
+}).listen([6002], [6001], function onListening() {
 	console.log("SERVERIS IJUNGTAS!");
 });
 
-var io = require('socket.io')(servers.tlsServers[0]);
+var io = require('socket.io')(servers.plainServers[0]);
 var Redis = require('ioredis');
 var redis = new Redis({
+	password: redisConfig.password
+});
+var redisServer = new Redis({
 	password: redisConfig.password
 });
 var Imagemin = require('imagemin');
 var path = require('path');
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://maze.lt');
+    res.setHeader('Access-Control-Allow-Origin', 'https://maze.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'PATCH', 'PUT', 'DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -34,9 +37,12 @@ redis.psubscribe('*', function(err, count) {
 });
 
 redis.on('pmessage', function(subscribed, channel, event) {
+	console.log('message received');
     event = JSON.parse(event);
-	if(event.event == 'maze\\Events\\AvatarWasUploaded')
-	{
+    console.log(subscribed);
+    console.log(event);
+    console.log(channel);
+	if(channel == 'avatars') {
 		var dest = path.dirname(event.data.path);
 		new Imagemin()
 		.use(Imagemin.optipng({optimizationLevel: 3}))
@@ -46,15 +52,42 @@ redis.on('pmessage', function(subscribed, channel, event) {
 			if(err)
 				console.log(err);
 		});
-	}
-	else
-	{
+	} else if(channel == 'notifications') {
+		io.sockets.in(event.data.user.secret).emit('notification', event.data.notification);
+	} else {
 	    io.sockets.in(event.data.channel).emit('message', event.data.message);
 	}
 });
 
+/**
+ * Kiekvienas connection turi savo scope.
+ * Viskas, kas yra connection funkcijoje yra kuriama atskirai kiekvienam socketo prisijungimui.
+ * Potencialiai gali valgyti daug atminties.
+ */
 io.sockets.on('connection', function (socket) {
-  socket.on('join', function (data) {
-    socket.join(data.id + '-' + data.secret);
-  });
+	var secret;
+
+	// Prisijungimas į savo secret kanalą.
+	socket.on('join', function(channel) {
+		if(channel.type == 'user') {
+			console.log(channel.token + ' connected');
+			secret = channel.token;
+			redisServer.sadd('online_users', secret);
+			socket.join(secret);
+		} else if (channel.type == 'message') {
+			console.log('joining message channel');
+
+		}
+	});
+
+	socket.on('disconnect', function() {
+		console.log(secret + 'disconnected');
+		redisServer.srem('online_users', secret);
+	});
+
+
+	//TODO: Chat
+	// socket.on('join', function (data) {
+	// 	socket.join(data.id + '-' + data.secret);
+	// });
 });
