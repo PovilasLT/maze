76e2c6cddfbfc2cce13a61bcab2cf7b65d0aae61
SkepticalHippo <edvinasrp@gmail.com>
2016-05-03 00:06:32 +0100

    HTTPS NS

--- notification.server.js
+++ notification.server.js
@@ -5,17 +5,17 @@
  */
 
 var lex = require('letsencrypt-express');
+var fs = require('fs');
 var express = require('express');
 var app = express();
+var https = require('https');
+var keys = {
+		key: fs.readFileSync('/etc/nginx/ssl/maze.lt/87263/server.key'),
+		cert: fs.readFileSync('/etc/nginx/ssl/maze.lt/87263/server.crt')
+};
+var server = https.createServer(keys, app).listen(6001);
 
-var servers = lex.create({
-	configDir: '/etc/letsencrypt',
-	onRequest: app,
-}).listen([], [6001], function onListening() {
-	console.log("SERVERIS IJUNGTAS!");
-});
-
-var io = require('socket.io')(servers.tlsServers[0]);
+var io = require('socket.io')(server);
 var Redis = require('ioredis');
 var redis = new Redis();
 var redisServer = new Redis();
@@ -23,24 +23,20 @@ var Imagemin = require('imagemin');
 var path = require('path');
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://maze.lt');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'PATCH', 'PUT', 'DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-app.get('/', function (req, res) {
-  res.send('...');
+	res.setHeader('Access-Control-Allow-Origin', 'https://maze.lt');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'PATCH', 'PUT', 'DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
 });
 
 redis.psubscribe('*', function(err, count) {
-	if(err) console.log(err);
+		if(err) console.log(err);
 });
 
 redis.on('pmessage', function(subscribed, channel, event) {
-    event = JSON.parse(event);
-	io.sockets.in(event.data.channel).emit(channel, event.data.data);
+	event = JSON.parse(event);
+		io.sockets.in(event.data.channel).emit(channel, event.data.data);
 });
 
 /**
@@ -49,16 +45,16 @@ redis.on('pmessage', function(subscribed, channel, event) {
  * Potencialiai gali valgyti daug atminties.
  */
 io.sockets.on('connection', function (socket) {
-	var secret;
+		var secret;
 
-	// Prisijungimas į savo secret kanalą.
-	socket.on('join', function(channel) {
-		secret = channel;
-		socket.join(channel);
-		redisServer.sadd('online_users', secret);
-	});
+		// Prisijungimas į savo secret kanalą.
+		socket.on('join', function(channel) {
+				secret = channel;
+				socket.join(channel);
+				redisServer.sadd('online_users', secret);
+		});
 
-	socket.on('disconnect', function() {
-		redisServer.srem('online_users', secret);
-	});
+		socket.on('disconnect', function() {
+				redisServer.srem('online_users', secret);
+		});
 });
