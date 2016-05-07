8634c77114516633c8d5461efa135f4318691c0a
SkepticalHippo <edvinasrp@gmail.com>
2016-05-07 20:13:30 +0100

    a button -> a

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,10 +1,8 @@
 @if(isset($node))
-<a href="{{ route('topic.create', ['skiltis' => $node->id]) }}">
-<button type="button" class="btn btn-success full-width new-topic"><i class="fa fa-plus-square"></i> Kurti naują temą</button>
+<a href="{{ route('topic.create', ['skiltis' => $node->id]) }}" class="btn btn-success full-width new-topic"><i class="fa fa-plus-square"></i> Kurti naują temą
 </a>
 @else
-<a href="{{ route('topic.create') }}">
-<button type="button" class="btn btn-success full-width new-topic"><i class="fa fa-plus-square"></i> Kurti naują temą</button>
+<a href="{{ route('topic.create') }}" class="btn btn-success full-width new-topic"><i class="fa fa-plus-square"></i> Kurti naują temą
 </a>
 @endif
 @include('panels.nodes', ['front_page_nodes' => Auth::check() ? Auth::user()->frontPageNodes()->toArray() : false ])
