ba20ae783c19e8ba7ee3337f8c5b656b3bc0c593
SkepticalHippo <edvinasrp@gmail.com>
2016-05-07 15:44:11 +0100

    slug/id bidning atskyrimas.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -25,33 +25,43 @@ class RouteServiceProvider extends ServiceProvider
     {
         parent::boot($router);
         
-        $router->bind('user', 'maze\User', function ($value) {
+        /**
+         * Misc. Bindings.
+         */
+        $router->bind('user-slug', 'maze\User', function ($value) {
             $user = maze\User::where('slug', $value)->first();
             if ($user) {
                 return $user;
             }
             abort(404);
         });
-        $router->bind('topic', function ($value) {
+        $router->bind('topic-slug', function ($value) {
             $topic = maze\Topic::where('slug', $value)->first();
             if ($topic) {
                 return $topic;
             }
             abort(404);
         });
-        $router->bind('node', function ($value) {
+        $router->bind('node-slug', function ($value) {
             $node = maze\Node::where('slug', $value)->first();
             if ($node) {
                 return $node;
             }
             abort(404);
         });
-        $router->model('reply', 'maze\Reply');
-        $router->model('status', 'maze\Status');
-        $router->model('status_comment', 'maze\StatusComment');
-        $router->model('notification', 'maze\Notification');
-        $router->model('conversation', 'maze\Messenger\Conversation');
-        $router->model('message', 'maze\Messenger\Message');
+
+        /**
+         * ID Bindings
+         */
+        $router->model('node-id', 'maze\Node');
+        $router->model('user-id', 'maze\User');
+        $router->model('topic-id', 'maze\Topic');
+        $router->model('reply-id', 'maze\Reply');
+        $router->model('status-id', 'maze\Status');
+        $router->model('statuscomment-id', 'maze\StatusComment');
+        $router->model('notification-id', 'maze\Notification');
+        $router->model('conversation-id', 'maze\Messenger\Conversation');
+        $router->model('message-id', 'maze\Messenger\Message');
     }
 
     /**
