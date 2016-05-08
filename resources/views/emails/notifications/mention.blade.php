dfc525892d2d49ab34c2f4e693b98444e9ba8169
SkepticalHippo <edvinasrp@gmail.com>
2016-05-08 14:38:10 +0100

    Paminejimo email teksto pataisymas.

--- resources/views/emails/notifications/mention.blade.php
+++ resources/views/emails/notifications/mention.blade.php
@@ -2,7 +2,7 @@
 <p>Tave paminėjo temoje <a href="">{{ $title }}</a>:</p>
 <h2>{{ $title }}</h2>
 {!! $content !!}
-<p><a href="{{ route('topic.show', [$slug, $id]) }}">Perskaityti ir atsakyti į temą.</a></p>
+<p><a href="{{ route('topic.show', [$slug, '#pranesimas-'.$id]) }}">Perskaityti ir atsakyti į temą.</a></p>
 <p><b>Dėmesio!</b> Po šio laiško, tema gali turėti ir daugiau naujų pranešimų.</p>
 <p>Pagarbiai,<br>
 	Maze Administracija</p>
